0ecbb444bcc12778e1ee91d0b791eea6aad41b68
Arjun <247arjun@gmailcom>
2020-05-30 14:20:36 -0700

    Initial push

---
+++ Faceblock/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Faceblock
+//
+//  Created by Arjun Gopalakrishna on 15-May-20.
+//  Copyright © 2020 Arjun G. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class ViewController: UIViewController {
+    //MARK: Properties
+
+    @IBOutlet weak var ruleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
